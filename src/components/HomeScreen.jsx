4ce474cf3c257e141b6d59d3f9cb36a8f9b30c38
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-09-02 19:17:07 +0530

    add tailwind config

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -10,7 +10,9 @@ const HomeScreen = () => {
     return (
         <>
             <div>HI frans</div>
-            <button onClick={handleLogout}>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={handleLogout}>
                 Logout
             </button>
         </>
